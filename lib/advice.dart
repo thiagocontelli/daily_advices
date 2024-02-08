d91fbf521330e9c1f3ecfa015bc91032f4e684b6
Thiago Contelli <thiagocontellid@gmail.com>
2024-02-08 00:53:47 -0300

    implement bottom navigation

---
+++ lib/advice.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Advice> fetchAdvice() async {
+  final response =
+      await http.get(Uri.parse('https://api.adviceslip.com/advice'));
+
+  if (response.statusCode == 200) {
+    return Advice.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed.');
+  }
+}
+
+class Slip {
+  final int id;
+  final String advice;
+
+  const Slip({required this.id, required this.advice});
+
+  factory Slip.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'id': int id, 'advice': String advice} => Slip(id: id, advice: advice),
+      _ => throw const FormatException('Failed to load slip.'),
+    };
+  }
+}
+
+class Advice {
+  final Slip slip;
+
+  const Advice({
+    required this.slip,
+  });
+
+  factory Advice.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'slip': Map<String, dynamic> slip} => Advice(
+          slip: Slip.fromJson(slip),
+        ),
+      _ => throw const FormatException('Failed to load advice.'),
+    };
+  }
+}
+
+class AdviceScreen extends StatefulWidget {
+  const AdviceScreen({super.key});
+
+  @override
+  State<AdviceScreen> createState() => _AdviceScreenState();
+}
+
+class _AdviceScreenState extends State<AdviceScreen> {
+  late Future<Advice> futureAdvice;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAdvice = fetchAdvice();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Flex(
+        direction: Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Wrap(
+            runSpacing: 32,
+            children: [
+              const Center(
+                child: Text('This is your daily advice:'),
+              ),
+              FutureBuilder<Advice>(
+                future: futureAdvice,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Center(
+                        child: Text(
+                      '"${snapshot.data!.slip.advice}"',
+                      style: const TextStyle(
+                          fontSize: 24, fontStyle: FontStyle.italic),
+                      textAlign: TextAlign.center,
+                    ));
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('${snapshot.error}'));
+                  }
+
+                  return const Center(
+                    child: Text('Loading...'),
+                  );
+                },
+              ),
+              const Center(
+                child: Text('Come back tomorrow for more advices!'),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,5 @@
-import 'dart:convert';
-
+import 'package:daily_advices/advice.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-Future<Advice> fetchAdvice() async {
-  final response =
-      await http.get(Uri.parse('https://api.adviceslip.com/advice'));
-
-  if (response.statusCode == 200) {
-    return Advice.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  } else {
-    throw Exception('Failed.');
-  }
-}
-
-class Slip {
-  final int id;
-  final String advice;
-
-  const Slip({required this.id, required this.advice});
-
-  factory Slip.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {'id': int id, 'advice': String advice} => Slip(id: id, advice: advice),
-      _ => throw const FormatException('Failed to load slip.'),
-    };
-  }
-}
-
-class Advice {
-  final Slip slip;
-
-  const Advice({
-    required this.slip,
-  });
-
-  factory Advice.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {'slip': Map<String, dynamic> slip} => Advice(
-          slip: Slip.fromJson(slip),
-        ),
-      _ => throw const FormatException('Failed to load advice.'),
-    };
-  }
-}
 
 void main() {
   runApp(const MainApp());
@@ -57,59 +13,37 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  late Future<Advice> futureAdvice;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAdvice = fetchAdvice();
-  }
+  int index = 0;
+  final List<Widget> screens = [
+    const AdviceScreen(),
+    const Center(
+      child: Text('Work in progress...'),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: Scaffold(
-        body: SafeArea(
-          child: Flex(
-            direction: Axis.vertical,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Wrap(
-                runSpacing: 32,
-                children: [
-                  const Center(
-                    child: Text('This is your daily advice:'),
-                  ),
-                  FutureBuilder<Advice>(
-                    future: futureAdvice,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Center(
-                            child: Text(
-                          '"${snapshot.data!.slip.advice}"',
-                          style: const TextStyle(
-                              fontSize: 24, fontStyle: FontStyle.italic),
-                          textAlign: TextAlign.center,
-                        ));
-                      } else if (snapshot.hasError) {
-                        return Center(child: Text('${snapshot.error}'));
-                      }
-
-                      return const Center(
-                        child: Text('Loading...'),
-                      );
-                    },
-                  ),
-                  const Center(
-                    child: Text('Come back tomorrow for more advices!'),
-                  )
-                ],
-              )
-            ],
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: index,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.history_edu), label: 'Advice'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.history), label: 'History'),
+              ],
+              onTap: (newIndex) => {
+                setState(() {
+                  index = newIndex;
+                })
+              },
+            ),
+            body: screens[index],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
